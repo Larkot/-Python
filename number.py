1bd8ef18406f7b5e54b088867428a83f610e3425
Larkot <39681012+Larkot@users.noreply.github.com>
2020-09-26 15:58:54 +0300

    Delete number.py

--- number.py
+++
@@ -1,5 +0,0 @@
-number = input("Please enter a number: ")
-second_number = number+number
-third_number = number + number +number
-result = int(number)+int(second_number)+int(third_number)
-print(result)
