0d7cda8d8feeef169ec8c809fd5bd401c520ea8d
Larkot <39681012+Larkot@users.noreply.github.com>
2020-10-03 18:33:00 +0300

    Delete lesson3_2.py

--- lesson3_2.py
+++
@@ -1,4 +0,0 @@
-def info(name,surname,year,city,email,phone):
-    print(f"{name},{surname},{year},{city},{email},{phone}")
-
-print(info(name="Mikle",surname="Kondratenko",year=1992,city="Dnepr",email="424242@mail.ru",phone = 35324523445))
