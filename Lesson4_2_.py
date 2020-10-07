07b4b8532d371a1914593dd267a7ffceda881d86
Larkot <39681012+Larkot@users.noreply.github.com>
2020-10-07 15:55:31 +0300

    Delete Lesson4_2_.py

--- Lesson4_2_.py
+++
@@ -1,4 +0,0 @@
-list_numbers = [13,4,8,7,11,6,5]
-new_list_number = [el for num,el in enumerate(list_numbers) if list_numbers[num-1]<list_numbers[num]]
-print(f"{list_numbers}")
-print(f"{new_list_number}")
