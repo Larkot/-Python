fd20cafc9e87a6214f57b4dc0bbfb39ab156eb22
Larkot <39681012+Larkot@users.noreply.github.com>
2020-10-12 15:29:20 +0300

    Delete lesson4_5.py

--- lesson4_5.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-
-
-def some_func(one_elem, selecon_elem):
-    return one_elem * selecon_elem
-
-print(f' {reduce(some_func, [el for el in range(99, 1001) if el % 2 == 0])}')
